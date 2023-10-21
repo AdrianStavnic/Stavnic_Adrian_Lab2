29f5836856fa1f0de3c7975fe6651510bcf79a2f
ADRIAN STAVNIC <adrian.stavnic@stud.ubbcluj.ro>
2023-10-21 16:31:37 +0300

    Add project files.

---
+++ Data/Stavnic_Adrian_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stavnic_Adrian_Lab2.Models;
+
+namespace Stavnic_Adrian_Lab2.Data
+{
+    public class Stavnic_Adrian_Lab2Context : DbContext
+    {
+        public Stavnic_Adrian_Lab2Context (DbContextOptions<Stavnic_Adrian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stavnic_Adrian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Stavnic_Adrian_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Stavnic_Adrian_Lab2.Models.Author>? Authors { get; set; }
+    }
+}
