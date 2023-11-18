12e134323c984dd5b355527ec18e69a1f893fe93
ADRIAN STAVNIC <adrian.stavnic@stud.ubbcluj.ro>
2023-11-18 21:49:41 +0200

    Laborator6 + Sarcini

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using Stavnic_Adrian_Lab2.Models;
 
 namespace Stavnic_Adrian_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel : BookCategoriesPageModel
     {
         private readonly Stavnic_Adrian_Lab2.Data.Stavnic_Adrian_Lab2Context _context;
